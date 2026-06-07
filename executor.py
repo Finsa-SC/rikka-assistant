837c5e8f7702886274ac9c22a4ac4bb41423a7ad
Finsa-SC <finsakusumaputra@gmail.com>
2026-06-07 14:18:26 +0700

    feat(executor): add background and foreground execution

--- executor.py
+++ executor.py
@@ -33,31 +33,57 @@ class CommandExecutor:
         first_word = command.strip().split()[0].lower()
         return first_word not in self.denied_command
 
-    def execute_commands(self, command: str) -> str:
+    def execute_commands(self, cmd_obj: dict) -> str:
+        command = cmd_obj["cmd"]
+        mode = cmd_obj.get("mode", "foreground")
+
         if not self.filter_commands(command):
             log.warning(f"Forbidden executing: {command}")
             return f"Forbidden command {command}"
+
+        if mode == "background":
+            return self._run_detached(command)
+        else:
+            return self._run_foreground(command)
+
+    @staticmethod
+    def _run_detached(self, command: str) -> str:
+        try:
+            log.info(f"Launched in background: {command}")
+            proc = subprocess.Popen(
+                command,
+                stdin=subprocess.DEVNULL,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                shell=True,
+                start_new_session=True,
+            )
+            return f"Launch in background (PID {proc.pid})"
+        except Exception as e:
+            log.error(f"Error while launching detached command: {e}")
+            return f"Error launching command: {e}"
+
+    @staticmethod
+    def _run_foreground(self, command: str, timeout: float = 60) -> str:
         try:
             result = subprocess.run(
                 command,
-                timeout=20,
+                timeout=timeout,
                 text=True,
-                capture_output=True,
                 shell=True,
+                capture_output=True
             )
-
             if result.returncode == 0:
                 return result.stdout.strip() if result.stdout.strip() else "Success without output"
             else:
-                log.error(f"Error occured while executing command")
-                return f"Error occured while executing command: \n{result.stderr.strip()}"
+                log.error(f"Command error: {result.stderr}")
+                return f"Error:\n{result.stderr.strip()}"
         except subprocess.TimeoutExpired:
-            log.error(f"Command timeout while it is running")
-            return f"Command timeout while it is running"
+            log.error(f"Command timeout: {command}")
+            return f"Command timed out after {timeout}s"
         except Exception as e:
-            log.error(f"Error occured while executing command: {e}")
-            return f"Error occured while executing command: {e}"
-
+            log.error(f"Error: {e}")
+            return f"Error: {e}"
 
 if __name__ == "__main__":
     executor = CommandExecutor()
